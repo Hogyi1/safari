ac183ec48fb4c1d5e7aaf3a3598bd6fa2a1a280f
Hogyi1 <hogyor.peter@gmail.com>
2025-04-14 15:15:42 +0200

    Restructured view classes for Structures, Roads, and Vegetation

---
+++ Assets/_Scripts/View/Building/VegetationView.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VegetationView : MonoBehaviour, IInteractable, IStageable, IPlaceable
+{
+#warning Not final implementation, might change in the future
+
+    private Structure MySelectable;
+    private bool isActive = false;
+    // Fade effekt komponens, ami elhalványítja ha rávisszük az egeret
+    private FadeEffect fadeEffect;
+    private StageEffect stageEffect;
+
+    [SerializeField] private float MyStage = 0f;
+
+    private void Update()
+    {
+        if (GetStage() != MyStage)
+        {
+            SetStage(GetStage());
+            MyStage = GetStage();
+            Debug.Log(GetStage());
+        }
+    }
+
+    // Lekéri a komponenseket
+    private void Awake()
+    {
+        fadeEffect = GetComponent<FadeEffect>();
+        stageEffect = GetComponent<StageEffect>();
+    }
+
+    // Inicializálja a view-t a kapcsolódó modell adattal
+    public void Init(Structure selectable)
+    {
+        this.MySelectable = selectable;
+        isActive = false;
+        fadeEffect.FadeOut();
+    }
+
+
+    // Egér rámutatás esemény kezelése (fade in effekt)
+    public void OnHover()
+    {
+        fadeEffect.FadeIn();
+    }
+
+    // Egér elhagyás esemény, ha nem aktív (fade out)
+    public void OnExit()
+    {
+        if (!isActive)
+        {
+            fadeEffect.FadeOut();
+        }
+    }
+
+    // Kattintás vagy aktiválás kezelése (fade in)
+    public void OnAction()
+    {
+        isActive = true;
+        fadeEffect.FadeIn();
+        PopupManager.Instance.ActivateStructurePopup(((ISelectable)MySelectable).GetUIData(), GetGameObject());
+    }
+
+    // Interakció megszüntetése, állapot alaphelyzetbe (fade out)
+    public void OnCancel()
+    {
+        isActive = false;
+        fadeEffect.FadeOut();
+    }
+
+    public GameObject GetGameObject()
+    {
+        return gameObject;
+    }
+
+    // Visszaadja a modell azonosítóját
+    public int GetID()
+    {
+        return MySelectable.GetID();
+    }
+
+    public BuildingType GetBuildingType()
+    {
+        return MySelectable.GetBuildingType();
+    }
+
+    public float GetStage()
+    {
+        if (MySelectable is Vegetation vegetation)
+        {
+            return vegetation.GetStage();
+        }
+        return 0;
+    }
+
+    public void SetStage(float stage)
+    {
+        stageEffect.SetMaterialsOpacity(stage);
+    }
+
+}
