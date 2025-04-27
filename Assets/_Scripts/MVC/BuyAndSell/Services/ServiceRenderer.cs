309be212a7e80dd251062b183e1c28972e2ed482
Zsigus Bence <ieemzi@inf.elte.hu>
2025-04-27 14:43:25 +0000

    Feature/shop inventory

---
+++ Assets/_Scripts/MVC/BuyAndSell/Services/ServiceRenderer.cs
@@ -0,0 +1,23 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+public class ServiceRenderer : MonoBehaviour
+{
+
+    [Header("TextFields")]
+    public TMP_Text priceHunter;
+    public TMP_Text priceDoctor;
+    public TMP_Text monthlySaleryHunter;
+    public TMP_Text monthlySaleryDoctor;
+    public TMP_Text monyAfterSalerys;
+    public TMP_Text NextPayment;
+    public TMP_Text monyEarned;
+    public TMP_Text animalsSaved;
+    public TMP_Text enemysKilled;
+    [Header("Buttons")]
+    public Button IncraseHunter;
+    public Button DecreaseHunter;
+    public Button IncraseDoctor;
+    public Button DecreaseDoctor;
+
+}
