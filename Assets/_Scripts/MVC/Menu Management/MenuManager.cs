fee0f07aac60b0437d36506ea97dbc31f802dbea
Tóth Balázs <k09fm2@inf.elte.hu>
2025-04-28 02:39:59 +0200

    Added function to change between sidemenu panels

--- Assets/_Scripts/MVC/Menu Management/MenuManager.cs
+++ Assets/_Scripts/MVC/Menu Management/MenuManager.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
+    [SerializeField] GameObject[] panels;
+
     public void ShowMenu(GameObject menu)
     {
         menu.SetActive(true);
@@ -10,5 +12,18 @@ public class MenuManager : MonoBehaviour
     public void HideMenu(GameObject menu)
     {
         menu.SetActive(false);
+    }
+
+    /// <summary>
+    /// Disables all panels in panels list, then enables the selected one.
+    /// </summary>
+    /// <param name="activePanel"></param>
+    public void SideMenuNavigationClick(GameObject activePanel)
+    {
+        foreach (GameObject panel in panels)
+        {
+            panel.SetActive(false);
+        }
+        activePanel.SetActive(true);
     }
 }
