ef29a58d1446d786ee195c77c35e0b72a2d24b6e
Tóth Balázs <k09fm2@inf.elte.hu>
2025-05-12 19:18:58 +0200

    Created 15 challenges

--- Assets/_Scripts/MVC/Challenges/EventType.cs
+++ Assets/_Scripts/MVC/Challenges/EventType.cs
@@ -4,20 +4,36 @@
 public enum EventType
 {
     /// <summary>An animal has been purchased.</summary>
-    BUY_ANIMAL,
-    /// <summary>An animal has been sold.</summary>
-    SELL_ANIMAL,
+    ANIMAL_BUY,
+    /// <summary>An animal has been killed.</summary>
+    ANIMAL_KILL,
+    /// <summary>An animal ate something.</summary>
+    ANIMAL_FED,
+    /// <summary>An animal gave birth.</summary>
+    ANIMAL_BORN,
+    /// <summary>The current animal count.</summary>
+    ANIMALS_OWNED,
     /// <summary>A tree has been placed.</summary>
-    PLACE_TREE,
+    TREE_PLACE,
     /// <summary>A building has been placed.</summary>
-    PLACE_BUILDING,
+    BUILDING_PLACE,
     /// <summary>A building has been upgraded.</summary>
-    UPGRADE_BUILDING,
-    /// <summary>Money has been earned.</summary>
-    EARN_MONEY,
+    BUILDING_UPGRADE,
+    /// <summary>A feeder/water dispenser has been placed.</summary>
+    FEEDER_PLACE,
+    /// <summary>A jeep has been purchased.</summary>
+    JEEP_BUY,
+    /// <summary>Money has been gained.</summary>
+    MONEY_GAIN,
     /// <summary>Experience points have been gained.</summary>
     EXP_GAIN,
+    /// <summary>Visitor has been transported.</summary>
+    VISITOR_TRANSPORTED,
+    /// <summary>Visitor happiness is high.</summary>
+    VISITOR_HAPPINESS_HIGH,
     /// <summary>Player leveled up.</summary>
     LEVEL_UP,
+    /// <summary>All challenges have been completed.</summary>
+    ALL_CHALLENGES_COMPLETED
     // Add more if we need to
 }
