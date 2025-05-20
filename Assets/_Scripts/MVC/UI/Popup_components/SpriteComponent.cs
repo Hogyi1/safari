01066ddb61278cbcb48162cf76556121d4183511
Hogyi1 <hogyor.peter@gmail.com>
2025-05-20 13:43:23 +0200

    Update UI components to reflect new data structure and economy state handling

--- Assets/_Scripts/MVC/UI/Popup_components/SpriteComponent.cs
+++ Assets/_Scripts/MVC/UI/Popup_components/SpriteComponent.cs
@@ -3,11 +3,26 @@ using UnityEngine;
 using UnityEngine.UI;
 using static UIKeys;
 
+/// <summary>
+/// A popup UI component responsible for displaying a sprite (e.g., icon).
+/// Implements the IPopupComponent interface for dynamic UI setup.
+/// </summary>
 public class SpriteComponent : MonoBehaviour, IPopupComponent
 {
+    /// <summary>
+    /// Reference to the UI Image component where the sprite will be rendered.
+    /// </summary>
     [SerializeField] private Image image;
+
+    /// <summary>
+    /// The key used to retrieve the sprite from the popup data dictionary.
+    /// </summary>
     [SerializeField] private UIKeys spriteKey = Sprite_icon;
 
+    /// <summary>
+    /// Attempts to assign a sprite from the provided data dictionary based on the spriteKey.
+    /// </summary>
+    /// <param name="data">Dictionary of popup UI data keyed by UIKeys enum.</param>
     public void TrySetup(Dictionary<UIKeys, object> data)
     {
         if (data.TryGetValue(spriteKey, out var icon))
@@ -15,5 +30,10 @@ public class SpriteComponent : MonoBehaviour, IPopupComponent
             image.sprite = (Sprite)icon;
         }
     }
+
+    /// <summary>
+    /// Called when the popup updates dynamically.
+    /// Currently not used by this component.
+    /// </summary>
     public void OnPopupUpdate() { }
 }
